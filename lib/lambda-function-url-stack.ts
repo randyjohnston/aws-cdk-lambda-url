3f91dfc32274d3096b6dfdcf7727b81e9099744c
Randy Johnston <randy.johnston@snyk.io>
2022-04-29 12:37:28 -0500

    Init with API-G methods GET,POST,DELETE

--- lib/lambda-function-url-stack.ts
+++ lib/lambda-function-url-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as widget_service from '../lib/widget_service';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaFunctionUrlStack extends Stack {
@@ -7,7 +8,7 @@ export class LambdaFunctionUrlStack extends Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
+    new widget_service.WidgetService(this, 'Widgets');
     // example resource
     // const queue = new sqs.Queue(this, 'LambdaFunctionUrlQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
